ca53a7856302fa35d5a2ef70d4d74af3d81aa810
thlorenz <thlorenz@gmx.de>
2014-09-09 15:37:38 +0100

    logging node version in sample scripts

--- example/greeter.js
+++ example/greeter.js
@@ -4,6 +4,8 @@ var binding = require('nad-bindings')('greeter');
 
 var greetme = module.exports = binding.greetme;
 
+console.log('node version', process.version);
+
 // Example usage
 var greeting = greetme('You');
 console.log(greeting);
--- example/hello.js
+++ example/hello.js
@@ -2,4 +2,5 @@
 
 var binding = require('nad-bindings')('hello');
 
+console.log('node version', process.version);
 console.log('hello', binding.hello());
