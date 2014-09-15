37b3498d15fdbde5cffb697085426813ab5ddba7
thlorenz <thlorenz@gmx.de>
2014-09-15 15:20:44 -0500

    properly exposing nad command

--- bin/nad.sh
+++ bin/nad.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 CWD=`pwd`
-NAD_BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+NAD_BIN_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 config_mk=$CWD/.nadconfig.mk 
 
 function log() {
